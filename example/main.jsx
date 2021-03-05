4c5dff9c8f2a87c391512d4cc519593461211b9d
John Ray <john@trychec.com>
2021-03-05 02:01:36 -0500

    more example usecases

--- example/main.jsx
+++ example/main.jsx
@@ -11,10 +11,10 @@ function App() {
     <div className="p-10 bg-red-300">
       Components:
       <ul>
-        <li class="space-y-1">
+        <li>
           <p>Textfield component</p>
-          <TextField placeholder="" className="text-xs p-2 bg-red-100 rounded-md" value={textValue} onChange={(event) => setTextValue(event.target.value)} />
-          <TextField placeholder="" className="text-sm px-10 py-20 bg-black text-white rounded-md" label="password" type="password" value={textValue} onChange={(event) => setTextValue(event.target.value)} />
+          <TextField placeholder="" className="p-4 mt-10 mb-8 bg-red-100 rounded-md" inputClassName="text-xl" value={textValue} onChange={(event) => setTextValue(event.target.value)} />
+          <TextField placeholder="" className="text-sm px-10 py-20 mb-8 bg-black text-white rounded-md" label="password" type="password" value={textValue} onChange={(event) => setTextValue(event.target.value)} />
           <TextField placeholder="With Placeholder" className="px-4 py-10" labelClassName="text-sm" value={textValueWithPlaceholder} onChange={(event) => setTextValueWithPlaceholder(event.target.value)} />
         </li>
       </ul>

55ffaf69bb85f9da4c481e8c213499d84866b58f
John Ray <john@trychec.com>
2021-03-05 01:58:07 -0500

    show more usecases in example

--- example/main.jsx
+++ example/main.jsx
@@ -13,8 +13,9 @@ function App() {
       <ul>
         <li class="space-y-1">
           <p>Textfield component</p>
-          <TextField placeholder="" value={textValue} onChange={(event) => setTextValue(event.target.value)} />
-          <TextField placeholder="With Placeholder" value={textValueWithPlaceholder} onChange={(event) => setTextValueWithPlaceholder(event.target.value)} />
+          <TextField placeholder="" className="text-xs p-2 bg-red-100 rounded-md" value={textValue} onChange={(event) => setTextValue(event.target.value)} />
+          <TextField placeholder="" className="text-sm px-10 py-20 bg-black text-white rounded-md" label="password" type="password" value={textValue} onChange={(event) => setTextValue(event.target.value)} />
+          <TextField placeholder="With Placeholder" className="px-4 py-10" labelClassName="text-sm" value={textValueWithPlaceholder} onChange={(event) => setTextValueWithPlaceholder(event.target.value)} />
         </li>
       </ul>
     </div>
--- src/components/TextField.jsx
+++ src/components/TextField.jsx
@@ -2,20 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import '../assets/styles/textfield.css';
 
-export default function TextField({ id = '', className = '', type = 'text', onChange, value = '', disabled = false, label = "label", placeholder }) {
+export default function TextField({ id = '', className = '', inputClassName = '', labelClassName = '', type = 'text', onChange, value = '', disabled = false, label = "label", placeholder }) {
   // https://stackoverflow.com/a/65901743/8320709 placeholder trick
   function hasPlaceholder() {
     return placeholder !== undefined && placeholder.length > 0;
   };
   return (
     <div id={id} className={`text-field ${className} ${(disabled ? 'text-field__disabled' : '')}`}>
-      <input id="current-id" className={`text-field__input ${hasPlaceholder() ? 'text-field__input--has-placeholder' : 'text-field__input--has-no-placeholder'}`} type={type} value={value} onChange={onChange} disabled={disabled} placeholder={hasPlaceholder() ? placeholder : ' '} />
-      <label
-        className="text-field__label"
-        htmlFor="current-id"
-      >
-        { label }
-      </label>
+      <div className={`relative`}>
+        <input id="current-id" className={`text-field__input ${hasPlaceholder() ? 'text-field__input--has-placeholder' : 'text-field__input--has-no-placeholder'} ${inputClassName}`} type={type} value={value} onChange={onChange} disabled={disabled} placeholder={hasPlaceholder() ? placeholder : ' '} />
+        <label
+          className={`text-field__label ${labelClassName}`}
+          htmlFor="current-id"
+        >
+          { label }
+        </label>
+      </div>
     </div>
   )
 }

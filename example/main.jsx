548690eb1190e10bf34dd0ce9a454321f342b5ce
John Ray <john@trychec.com>
2021-03-19 02:36:19 -0400

    clean up

--- example/main.jsx
+++ example/main.jsx
@@ -8,16 +8,15 @@ function App() {
   // WIP 
   return (
     <div className="twr-p-10">
-      Components:
-      <ul>
+      <ul class="twr-space-y-5">
         <li class="twr-space-y-4">
-          <p>Textfield component</p>
+          <p>{`<TextField>`} component:</p>
           <TextField value={textValue} onChange={(event) => setTextValue(event.target.value)} />
           <TextField placeholder="placeholder" value={textValue} onChange={(event) => setTextValue(event.target.value)} />
 
         </li>
         <li class="twr-space-y-4">
-          <p>TextArea component</p>
+          <p>{'<TextArea>'} component:</p>
           <TextField type="textarea" value={textValue} onChange={(event) => setTextValue(event.target.value)} />
           <TextField type="textarea" placeholder="placeholder" value={textValue} onChange={(event) => setTextValue(event.target.value)} />
 
